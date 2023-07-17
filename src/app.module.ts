10287feb7f9079a7dcc7902f5dc70b504779fba2
Ivan <foxriver660@gmail.com>
2023-07-17 14:26:22 +0300

    feat: add basic frame

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { WishesModule } from './wishes/wishes.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+import { OffersModule } from './offers/offers.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, WishesModule, WishlistsModule, OffersModule],
   controllers: [AppController],
   providers: [AppService],
 })
