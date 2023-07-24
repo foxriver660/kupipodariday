8ed9b1a5da161985b0a3d07bb3b66d073e92fea5
Ivan <foxriver660@gmail.com>
2023-07-24 14:54:28 +0300

    fix: removecsrf

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
-import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use(helmet());
-  app.use(csurf());
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(4200);
 }
